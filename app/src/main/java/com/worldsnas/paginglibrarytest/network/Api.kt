7763c03a7b675ccb34effbc1f2b2dcfbd8c8607a
worldsnas <worldsnas@gmail.com>
2018-07-11 16:16:28 +0430

    auth headers added to api

--- app/src/main/java/com/worldsnas/paginglibrarytest/network/Api.kt
+++ app/src/main/java/com/worldsnas/paginglibrarytest/network/Api.kt
@@ -3,12 +3,14 @@ package com.worldsnas.paginglibrarytest.network
 import com.worldsnas.paginglibrarytest.network.model.Photo
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface Api {
 
+    @Headers("Authorization: Client-ID (access-token)")
     @GET("/photos")
-    fun getPopulartPhotos(@Query("page") page: Int = 1,
-                          @Query("per_page") perPage : Int = 10,
-                          @Query("order_by") orderBy : String = "popular") : Observable<List<Photo>>
+    fun getPopularPhotos(@Query("page") page: Int = 1,
+                         @Query("per_page") perPage : Int = 10,
+                         @Query("order_by") orderBy : String = "popular") : Observable<List<Photo>>
 }
--- app/src/main/java/com/worldsnas/paginglibrarytest/network/RestManager.kt
+++ app/src/main/java/com/worldsnas/paginglibrarytest/network/RestManager.kt
@@ -2,7 +2,6 @@ package com.worldsnas.paginglibrarytest.network
 
 import com.worldsnas.paginglibrarytest.network.model.Photo
 import io.reactivex.Observable
-import retrofit2.http.Query
 import javax.inject.Inject
 
 class RestManager @Inject constructor() {
@@ -10,10 +9,10 @@ class RestManager @Inject constructor() {
     @Inject
     lateinit var api: Api
 
-    fun getPopulartPhotos(page: Int = 1,
-                          perPage: Int = 10,
-                          orderBy: String = "popular"): Observable<List<Photo>> {
-        return api.getPopulartPhotos(page, perPage, orderBy)
+    fun getPopularPhotos(page: Int = 1,
+                         perPage: Int = 10,
+                         orderBy: String = "popular"): Observable<List<Photo>> {
+        return api.getPopularPhotos(page, perPage, orderBy)
     }
 
 }
