1939ab82e301bba24f9196c419064f87750cd8d0
worldsnas <worldsnas@gmail.com>
2018-07-11 16:12:23 +0430

    rest manager added to viewmodel

--- app/src/main/java/com/worldsnas/paginglibrarytest/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/worldsnas/paginglibrarytest/viewmodel/MainActivityViewModel.kt
@@ -1,7 +1,12 @@
 package com.worldsnas.paginglibrarytest.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import com.worldsnas.paginglibrarytest.network.RestManager
+import com.worldsnas.paginglibrarytest.network.SchedulersFacade
 import javax.inject.Inject
 
-class MainActivityViewModel @Inject constructor(): ViewModel() {
+class MainActivityViewModel @Inject constructor(val restManager: RestManager, val schedulersFacade: SchedulersFacade): ViewModel() {
+
+
+
 }
