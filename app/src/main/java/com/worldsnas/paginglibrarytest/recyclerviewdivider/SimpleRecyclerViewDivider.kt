9efbe36109a647128d8c80ca1e0961c35655e1ba
worldsnas <worldsnas@gmail.com>
2018-07-14 09:45:58 +0430

    recycler view divider created

---
+++ app/src/main/java/com/worldsnas/paginglibrarytest/recyclerviewdivider/SimpleRecyclerViewDivider.kt
@@ -0,0 +1,17 @@
+package com.worldsnas.paginglibrarytest.recyclerviewdivider
+
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class SimpleRecyclerViewDivider constructor(val left: Int = 0, val right: Int = 0,
+                                            val top: Int = 0, val bottom: Int = 0) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+        outRect?.top = top
+        outRect?.right = right
+        outRect?.left = left
+        outRect?.bottom = bottom
+        super.getItemOffsets(outRect, view, parent, state)
+    }
+}
--- app/src/main/java/com/worldsnas/paginglibrarytest/ui/MainActivity.kt
+++ app/src/main/java/com/worldsnas/paginglibrarytest/ui/MainActivity.kt
@@ -4,16 +4,21 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.worldsnas.paginglibrarytest.R
+import com.worldsnas.paginglibrarytest.recyclerviewdivider.SimpleRecyclerViewDivider
 import com.worldsnas.paginglibrarytest.viewmodel.MainActivityViewModel
 import com.worldsnas.paginglibrarytest.viewmodel.MainActivityViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import javax.inject.Provider
 
 class MainActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var viewModelProviderFactory: MainActivityViewModelFactory
+    @Inject
+    lateinit var photosAdapterProvider: Provider<PhotosAdapter>
+    lateinit var photosAdapter: PhotosAdapter
 
     lateinit var viewModel: MainActivityViewModel
 
@@ -23,10 +28,15 @@ class MainActivity : DaggerAppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this, viewModelProviderFactory).get(MainActivityViewModel::class.java)
 
+
+
         initRecycler()
     }
 
-    private fun initRecycler(){
+    private fun initRecycler() {
+        photosAdapter = photosAdapterProvider.get()
         recyclerMainPhotoList.layoutManager = LinearLayoutManager(this)
+        recyclerMainPhotoList.adapter = photosAdapter
+        recyclerMainPhotoList.addItemDecoration(SimpleRecyclerViewDivider(bottom = 16))
     }
 }
